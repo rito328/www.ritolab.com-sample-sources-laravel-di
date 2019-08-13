3e9297e7ae0b488957a796ecd704bae1b8700fe7
rito328 <ritolabo@gmail.com>
2019-08-13 23:03:18 +0900

    Added SampleService

---
+++ app/Services/SampleService.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Services;
+
+class SampleService
+{
+    protected $SampleComponent;
+
+    public function __construct(SampleInterface $SampleComponent)
+    {
+        $this->SampleComponent = $SampleComponent;
+    }
+
+    public function index()
+    {
+        $result = $this->SampleComponent->execute();
+
+        echo $result;
+    }
+}
