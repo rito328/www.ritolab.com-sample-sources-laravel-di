248725372d97cfb7a81b8bdff3d623ef3e260de6
rito328 <ritolabo@gmail.com>
2019-08-14 09:15:35 +0900

    Added CalculationServiceProvider

--- config/app.php
+++ config/app.php
@@ -175,6 +175,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /**
+         * Original Service Providers...
+         */
+        App\Providers\CalculationServiceProvider::class,
+
     ],
 
     /*
